c30768953717422e86d461e06c8226840e9f4f57
goz <u7126461@anu.edu.au>
2025-01-31 00:00:41 +1100

    Initial code

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import sys
+from rest_classes.message import Message
+import logging
+
+logger = logging.getLogger("uvicorn")
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/send")
+async def send(message: Message):
+    logger.info(f"### Recieved Message: {message.message}\n")
+    return {"message": "Recieved"}
+
+
