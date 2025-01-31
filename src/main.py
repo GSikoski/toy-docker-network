086af58e622b1dbf630068a5aea5f5b3a6100eb5
goz <goran.sikoski@Gmail.com>
2025-01-31 23:04:46 +1100

    Added docker compose and added new endpoint

--- src/main.py
+++ src/main.py
@@ -16,4 +16,9 @@ async def send(message: Message):
     logger.info(f"### Recieved Message: {message.message}\n")
     return {"message": "Recieved"}
 
+@app.post("/sendstr")
+async def send(message: str):
+    logger.info(f"### Recieved Message: {message}\n")
+    return {"message": "Recieved"}
+
 
