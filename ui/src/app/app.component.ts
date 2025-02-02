9b1c41e29d6a5a75f5d3317db41d47f157c00ea6
goz <goran.sikoski@Gmail.com>
2025-02-02 20:47:01 +1100

    Added some basic UI elements

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'ui';
+  userInput: string = '';
+
+  submitInput() {
+    console.log('User input submitted:', this.userInput);
+  }
 }
