1c8c56b4eb447609d8d63091f115315fc6548b19
goz <goran.sikoski@Gmail.com>
2025-02-03 22:05:40 +1100

    Implemented basic us with api call functionality

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Injectable  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,21 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
+@Injectable({
+  providedIn: 'root',
+})
 export class AppComponent {
   title = 'ui';
   userInput: string = '';
+
+  private apiUrl = 'http://127.0.0.1:8000/sendstr?message=' ;  // Replace with your API URL
+
+  constructor(private http: HttpClient) {};
+
+  sendMessage(){
+    let url:string  = this.apiUrl + this.userInput
+    this.http.post(url, null).subscribe(data => {console.log(data)});
+  }
 
   submitInput() {
     console.log('User input submitted:', this.userInput);
