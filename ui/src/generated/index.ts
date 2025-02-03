b7656bf8afe2d2b999f765e73088e43f0bd18876
goz <goran.sikoski@Gmail.com>
2025-02-03 23:00:00 +1100

    Implemented API refresh

---
+++ ui/src/generated/index.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { Body_login_for_access_token_token_post } from './models/Body_login_for_access_token_token_post';
+export type { HTTPValidationError } from './models/HTTPValidationError';
+export type { Message } from './models/Message';
+export type { Token } from './models/Token';
+export type { UserCreate } from './models/UserCreate';
+export type { ValidationError } from './models/ValidationError';
+
+export { DefaultService } from './services/DefaultService';
