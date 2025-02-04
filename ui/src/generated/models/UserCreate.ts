affc27b4428b395182f6ed4518ec7f9942840970
goz <goran.sikoski@Gmail.com>
2025-02-05 10:52:53 +1100

    Using generated functions now

--- ui/src/generated/models/UserCreate.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type UserCreate = {
-    name: string;
-    password: string;
-};
-
